796ddec4ee06dfe249400a7737407a508eb53974
Samit Jaiswal <samitjaiswal@gmail.com>
2023-03-09 10:03:13 +0530

    Payment page after checkout is added

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,9 +3,11 @@ import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
 import { getBasketTotal } from "./reducer";
+import { useNavigate } from "react-router-dom";
 
 function Subtotal() {
   const [{ basket }, dispatch] = useStateValue();
+  const history = useNavigate();
   return (
     <div className="subtotal">
       <CurrencyFormat
@@ -27,7 +29,7 @@ function Subtotal() {
         prefix={"₹"}
       />
 
-      <button>Proceed to Checkout</button>
+      <button onClick={(e) => history("/payment")}>Proceed to Checkout</button>
     </div>
   );
 }
