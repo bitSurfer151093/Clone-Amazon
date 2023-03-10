e0acb2b1f1fc101ebb2a3cc5cd74d601939d04e8
Samit Jaiswal <samitjaiswal@gmail.com>
2023-03-11 00:21:08 +0530

    Backend for Payment added with Payment Processing

---
+++ functions/index.js
@@ -0,0 +1,39 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const { request, response } = require("express");
+const stripe = require("stripe")(
+  "sk_test_51MjcgdSDibdwkhmJXKnbzfiCFxoBp3iUumUyz5DxVNwLLt2xB13ROp1zWIxjlC4x42dUXccf6Yv3NTLzM2s5i6Ms00gfQmbOkZ"
+);
+
+//API
+
+// App Config
+const app = express();
+
+//- Middleware
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+//- API Routes
+app.get("/", (request, response) => response.status(200).send("Ola World"));
+
+app.post("/payment/create", async (request, response) => {
+  const total = request.query.total;
+
+  console.log(" Payment request received >>> ", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "usd",
+  });
+
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// - Listen Commmand
+exports.api = functions.https.onRequest(app);
+
+//http://127.0.0.1:5001/clone-764d8/us-central1/api
--- src/Payment.js
+++ src/Payment.js
@@ -1,22 +1,59 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
 import CheckoutProduct from "./CheckoutProduct";
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import CurrencyFormat from "react-currency-format";
 import { getBasketTotal } from "./reducer";
+import axios from "./axios";
+import { useNavigate } from "react-router-dom";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
+  const history = useNavigate();
 
   const stripe = useStripe();
-  const element = useElements();
+  const elements = useElements();
 
-  const { error, setError } = useState(null);
-  const { disabled, setDisabled } = useState(true);
+  const [succeeded, setSucceeded] = useState(false);
+  const [processing, setProcessing] = useState("");
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState(true);
 
-  const handleSubmit = (e) => {};
+  useEffect(() => {
+    //get the secret phrases from bank using stripe to process the payment
+    const getClientSecret = async () => {
+      const response = await axios({
+        method: "post",
+        url: `/payment/create?total=${getBasketTotal(basket) * 100}`,
+      });
+      setClientSecret(response.data.clientSecret);
+    };
+
+    getClientSecret();
+  }, [basket]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setProcessing(true);
+
+    const payload = await stripe
+      .confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      })
+      .then(({ paymentIntent }) => {
+        //Stripe calls payment confirmation as payment intent, don't confuse this.
+        setSucceeded(true);
+        setError(null);
+        setProcessing(false);
+
+        history("/orders", { replace: true });
+      });
+  };
 
   const handleChange = (event) => {
     //Listen for the changes in the CardElement
@@ -74,7 +111,11 @@ function Payment() {
                   thousandSeperator={true}
                   prefix={"$"}
                 />
+                <button disabled={disabled || processing || succeeded}>
+                  <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                </button>
               </div>
+              {error && <div>{error}</div>}
             </form>
           </div>
         </div>
