659f467eeada09b9287dc3e66efac5e218339bc2
Samit Jaiswal <samitjaiswal@gmail.com>
2023-03-19 15:34:04 +0530

    The build is deployed on Firebase

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-764d8/us-central1/api", //The API (cloud function) URL
+  baseURL: "https://us-central1-clone-764d8.cloudfunctions.net/apicd",
+  //"http://127.0.0.1:5001/clone-764d8/us-central1/api", //The API (cloud function) URL
 });
 
 export default instance;
